ff6deb44758727f309493ea7c99f4c60a2c49d8d
erbdevs <erb.devs@gmail.com>
2022-04-26 22:20:02 +0200

    Basic ContactController#new view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,31 @@
 class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      # redirect_to @contact
+      render :new, status: :ok
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def index
     @all_contacts = Contact.all
+  end
+
+private
+
+  def contact_params
+    params.require(:contact).permit(
+      :first_name,
+      :last_name,
+      :phone,
+      :email
+    )
   end
 end
